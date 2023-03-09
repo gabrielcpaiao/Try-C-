fbb7bc4a21b22b6459e9b2e246ef7d6cb5d3ff80
Gabriel Paião <costapaiao@hotmail.com>
2023-03-09 20:51:30 -0300

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BondGarfoApp.Controllers;
+
+public class HelloWorldController : Controller
+{
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BondGarfoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
