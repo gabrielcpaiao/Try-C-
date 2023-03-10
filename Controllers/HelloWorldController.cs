66f7d0d0015f6d5f93f92974505d1c10bcc7600c
Gabriel Paião <costapaiao@hotmail.com>
2023-03-09 22:25:10 -0300

    Studying the structure of C#

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,5 +5,18 @@ namespace BondGarfoApp.Controllers;
 
 public class HelloWorldController : Controller
 {
+    //server/controlador/acao/id
+    //toda classe publica no controlador vira uma acao
+    public IActionResult Index()
+    {
+        return View();
+    }
 
+    public string Welcome(string nome, int ID=1)
+    {
+        return HtmlEncoder.Default.Encode(
+            //https://localhost:7037/HelloWorld/Welcome?nome=Gabriel&idade=24
+            $"Ola, {nome}. Seu ID e {ID}."
+        );
+    }
 }
